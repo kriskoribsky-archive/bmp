d91d674ffa08130d2674b9c235b449f103350244
kriskoribsky <kiko.koribsky@gmail.com>
2023-04-22 14:32:50 +0200

    add vertical flipping

--- ps4/main.c
+++ ps4/main.c
@@ -9,39 +9,31 @@ int main(void)
 {
 
     // FILE *fr = fopen("data/assets/square.1x1.bmp", "rb");
-    // FILE *fw = fopen("build/results/out/square.1x1OUT.bmp", "wb");
-
     // FILE *fr = fopen("data/assets/square.2x1.bmp", "rb");
-    // FILE *fw = fopen("build/results/out/square.2x1-horizontal-flip.bmp", "wb");
-
     // FILE *fr = fopen("data/assets/square.3x1.bmp", "rb");
-    // FILE *fw = fopen("build/results/out/square.3x1-horizontal-flip.bmp", "wb");
-
-    FILE *fr = fopen("data/assets/square.3x2.bmp", "rb");
-    FILE *fw = fopen("build/results/out/square.3x2-horizontal-flip.bmp", "wb");
-
+    // FILE *fr = fopen("data/assets/square.3x2.bmp", "rb");
     // FILE *fr = fopen("data/assets/square.4x4.bmp", "rb");
-    // FILE *fw = fopen("build/results/out/square.4x4OUT.bmp", "wb");
+    FILE *fr = fopen("data/assets/bmp.bmp", "rb");
 
     // FILE *fr = fopen("data/assets/saboteur.bmp", "rb");
-    // FILE *fw = fopen("build/results/out/saboteur-out.bmp", "wb");
-
     // FILE *fr = fopen("data/assets/adventurer.bmp", "rb");
-    // FILE *fw = fopen("build/results/out/adventurer-out.bmp", "wb");
-
     // FILE *fr = fopen("data/assets/prva.akcia.bmp", "rb");
-    // FILE *fw = fopen("build/results/out/prva.akcia-out.bmp", "wb");
+
+    FILE *fw = fopen("build/results/out/result.bmp", "wb");
 
     struct bmp_image *img = read_bmp(fr);
 
-    struct bmp_image *horizontal_flip = flip_horizontally(img);
+    // struct bmp_image *horizontal_flip = flip_horizontally(img);
+    struct bmp_image *vertical_flip = flip_vertically(img);
 
-    write_bmp(fw, horizontal_flip);
+    // write_bmp(fw, horizontal_flip);
+    write_bmp(fw, vertical_flip);
 
     fclose(fr);
     fclose(fw);
     free_bmp_image(img);
-    free_bmp_image(horizontal_flip);
+    // free_bmp_image(horizontal_flip);
+    free_bmp_image(vertical_flip);
 
     return EXIT_SUCCESS;
 }
--- ps4/transformations.c
+++ ps4/transformations.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "transformations.h"
 #include "bmp.h"
@@ -9,8 +10,8 @@ struct bmp_image *flip_horizontally(const struct bmp_image *image)
 
     struct bmp_image *copy = copy_bmp(image);
 
-    uint32_t width = image->header->width;
-    uint32_t height = image->header->height;
+    uint32_t width = copy->header->width;
+    uint32_t height = copy->header->height;
 
     // flipping horizontally means flipping along vertical axis
     for (uint32_t left_col = 0, right_col = width - 1; left_col < right_col; left_col++, right_col--)
@@ -24,6 +25,39 @@ struct bmp_image *flip_horizontally(const struct bmp_image *image)
             copy->data[offset + right_col] = tmp;
         }
     }
-
     return copy;
 }
+
+struct bmp_image *flip_vertically(const struct bmp_image *image)
+{
+    CHECK_NULL(image);
+
+    struct bmp_image *copy = copy_bmp(image);
+
+    uint32_t width = copy->header->width;
+    uint32_t height = copy->header->height;
+    size_t bytes = width * sizeof(struct pixel);
+
+    // flipping vertically means flipping along horizontal axis
+    // bmp images are stored in bottom to top order
+    for (uint32_t botom_row = 0, top_row = height - 1; botom_row < top_row; botom_row++, top_row--)
+    {
+        struct pixel *tmp = malloc(bytes);
+        if (tmp == NULL)
+        {
+            free_bmp_image(copy);
+            return NULL;
+        }
+
+        memcpy(tmp, &copy->data[botom_row * width], bytes);
+        memcpy(&copy->data[botom_row * width], &copy->data[top_row * width], bytes);
+        memcpy(&copy->data[top_row * width], tmp, bytes);
+
+        free(tmp);
+    }
+    return copy;
+}
+
+
+
+
